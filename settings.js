f3e1940a985700dc121dbf29e2f6ca595635b0d0
Hutta-k <huttak403@gmail.com>
2025-06-20 21:57:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,19 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || "ᴘᴜᴛ ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ ʜᴇʀᴇ."
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~H9ZXyBrI#UFNouNj_j9Hj23cDjGMr8qf_EfEkBAOZS8aSB3wRMSg"
 global.botname = process.env.BOTNAME || "KAVI-X MD"
-global.ownernumber = process.env.OWNERNUMBER || "94702128378"
-global.ownername = process.env.OWNERNAME || "Cyber Kavi"
+global.ownernumber = process.env.OWNERNUMBER || "9466384358"
+global.ownername = process.env.OWNERNAME || "🗿"
 global.websitex = process.env.WEBSITEX || "https://kavi-x-bot-login.up.railway.app/"
 global.wagc = process.env.WAGC || "https://chat.whatsapp.com/CG9f0paHJzwDImXiydfuht"
 global.botscript = process.env.BOTSCRIPT || "https://github.com/KaviDeveloperSe/KAVI-X-BOT"
 global.packname = process.env.PACKNAME || "KAVI-X MD"
-global.author = process.env.AUTHOR || "Cyber Kavi"
-global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
-global.botprefix = process.env.BOTPREFIX || "."
-global.restart = process.env.RESTART || true
-global.mongoDB = process.env.MONGODB_URI || "ᴘᴜᴛ ʏᴏᴜʀ ᴍᴏɴɢᴏᴅʙ ᴜʀɪ ʜᴇʀᴇ."
+global.author = process.env.AUTHOR || "🗿"
+global.creator = process.env.CREATOR || "94766384358@s.whatsapp.net"
+global.botprefix = process.env.BOTPREFIX || "/"
+global.restart = process.env.RESTART || "true"
+global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
